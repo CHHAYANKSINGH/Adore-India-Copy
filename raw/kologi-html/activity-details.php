36dc3902fa795dafae71f645fd015857cfcb586c
Chhayank <chhayank.singh2912@gmail.com>
2022-01-22 19:29:17 +0530

    pagination,activity detail

---
+++ raw/kologi-html/activity-details.php
@@ -0,0 +1,71 @@
+<?php include "layouts/header.php"; ?>
+<!-- Start main-content -->
+<div class="main-content-area">
+  <!-- Section: page title -->
+  <section class="page-title layer-overlay overlay-dark-9 section-typo-light bg-img-center" data-tm-bg-img="images/bg/bg1.jpg">
+    <div class="container pt-90 pb-90">
+      <div class="section-content">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h2 class="title">Activity Details</h2>
+            <nav class="breadcrumbs" role="navigation" aria-label="Breadcrumbs">
+              <div class="breadcrumbs">
+                <span><a href="#" rel="home">Home</a></span>
+                <span><i class="fa fa-angle-right"></i></span>
+                <span><a href="#">Pages</a></span>
+                <span><i class="fa fa-angle-right"></i></span>
+                <span class="active">Activity Details</span>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Section: Campaign details -->
+  <section>
+    <div class="container" align="center">
+      <div class="section-content">
+        <div class="col-lg-9">
+          <img alt="images" src="images/bg/activity1.jpg" />
+          <h3 class="mt-20 mb-10">Session by Mr. Dinesh Agarwal</h3>
+          <!-- <p class="lead">Lorem ipsum dolor sit amet <span class="text-theme-colored1 font-weight-600">Ecology</span>
+			 adipiscing elit. Etiam aliquet odio non porta laoreet. Vestibulum in dui euismod, molestie quam  -->
+          <span class="text-theme-colored1 font-weight-600">Report</span>
+          <p>The session began with a round of introductions of all the participants. Mr. Agarwal then dove into
+            his experiences as a young start-up founder and why failure is just a stepping stone to success.
+            His advice was to fail early and learn from our mistakes and then move onto
+            something which plays to our strengths. He then talked about his job experiences in the US and
+            why as Indians we should be proud to be a part of a nation that is fundamentally viewed as successful
+            and intelligent. Mr. Agarwal then talked about machine learning and artificial intelligence which
+            is his area of expertise. The discussion centered around the emerging importance of AI in every sector
+            and how we as students and young professionals can adapt ourselves to it. He very helpfully suggested
+            a few online courses which we can take up to know more. Mr. Agarwal then encouraged the participants
+            to ask him any question related to career development, technology and life abroad.</p>
+          <p> <span class="text-theme-colored1 font-weight-600">Impact</span> </p>
+          <p>Mr. Agarwal said that the western mentality of extreme individualism is not present in Indians
+            and we are always ready to help and support others.The session concluded with a round of thanks to
+            Mr. Amit Agarwal for his insight and to all the participants.</p>
+        </div>
+
+      </div>
+    </div>
+  </section>
+  <!-- End Divider -->
+</div>
+<!-- end main-content -->
+
+<!-- Footer -->
+<?php include "layouts/footer.php" ?>
+<a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
+</div>
+<!-- end wrapper -->
+
+<!-- Footer Scripts -->
+<!-- JS | Custom script for all pages -->
+<script src="js/custom.js"></script>
+
+</body>
+
+</html>
