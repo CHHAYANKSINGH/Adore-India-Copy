99a00c03196254b84d10d3ebc1da3df2fe0fbbcd
Aniket Singh <singhaniket1501@gmail.com>
2022-01-28 17:53:19 +0530

    news-details page added

---
+++ news-details.php
@@ -0,0 +1,76 @@
+<?php include "layouts/header.php"; ?>
+
+
+<div class="main-content-area">
+    <!-- Section: page title -->
+    <section class="page-title layer-overlay overlay-dark-9 section-typo-light bg-img-center" data-tm-bg-img="images/bg/bg1.jpg">
+        <div class="container pt-90 pb-90">
+            <div class="section-content">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <h2 class="title">Our Newsletters</h2>
+                        <nav class="breadcrumbs" role="navigation" aria-label="Breadcrumbs">
+                            <div class="breadcrumbs">
+                                <span><a href="#" rel="home">Home</a></span>
+                                <span><i class="fa fa-angle-right"></i></span>
+                                <span><a href="#">Pages</a></span>
+                                <span><i class="fa fa-angle-right"></i></span>
+                                <span class="active">News Details</span>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <div class="list">
+    <ul type="none">
+        <li>JULY 2, 2021</li>
+        <li>
+            <a href=#><h4>February 2021</h4></a></a>
+        </li><hr>
+        <li>FEBRUARY 22, 2021</li>
+        <li>
+        <a href=#><h4>January 2021</h4></a>
+        </li><hr>
+        <li>JANUARY 12, 2021</li>
+        <li>
+        <a href=#> <h4>December 2020</h4></a>
+        </li><hr>
+        <li>DECEMBER 10, 2020</li>
+        <li>
+        <a href=#><h4>November 2020</h4></a>
+        </li><hr>
+        <li> NOVEMBER 1, 2020</li>
+        <li>
+        <a href=#><h4>October 2020</h4></a>
+        </li><hr>
+        <li> NOVEMBER 1, 2020</li>
+        <li>
+        <a href=#><h4>October 2020</h4></a>
+        </li><hr>
+        <li> NOVEMBER 1, 2020</li>
+        <li>
+        <a href=#><h4>October 2020</h4></a>
+        </li><hr>
+        
+    </ul>
+    </div>
+   
+
+    </div>
+    <style>
+        .list{
+            margin: 60px 800px 60px 100px;
+            /* margin-left: 100px;
+            margin-right: 700px; */
+        }
+        </style>
+    </body>
+    <?php include "layouts/footer.php" ?>
+    <script src="js/custom.js"></script>
+
+    </html>
