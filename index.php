cb3e67b1663e77564e9ab65049441956b79f821d
Chhayank <chhayank.singh2912@gmail.com>
2022-02-01 23:28:57 +0530

    Linked Webinar API

--- index.php
+++ index.php
@@ -729,8 +729,8 @@
           </div>
         </div>
       </div>
-      <div class="row">
-        <div class="col-md-6 col-lg-4">
+      <div class="row" id="webinar">
+        <!-- <div class="col-md-6 col-lg-4">
           <div class="blog-current-style1 mb-lg-30">
             <article class="post">
               <div class="entry-header">
@@ -753,58 +753,100 @@
               </div>
             </article>
           </div>
-        </div>
-        <div class="col-md-6 col-lg-4">
-          <div class="blog-current-style1 mb-lg-30">
-            <article class="post">
-              <div class="entry-header">
-                <div class="post-thumb">
-                  <div class="post-thumb-inner">
-                    <div class="thumb" style="border-radius:15px 15px 0px 0px ;">
-                      <img class="w-100" src="images/blog/session2.jpeg" alt="Image" />
-                    </div>
+        </div> -->
+        <script>
+          // check if products are present in localstorage if not then fetch and set
+          if (localStorage.getItem("webinars") === null) {
+            fetch("https://api.adoreearth.org/webinars/").then((res) => {
+              return res.json();
+              // set this res.json in local storage
+            }).then((data) => {
+              // document.getElementsByClassName("preloader-floating-circles")[0].style.display = "none";
+              // data = _.orderBy(data, ['timestamp'], ['desc']);
+              localStorage.setItem('webinars', JSON.stringify(data));
+              console.log(data)
+              let data1 = "";
+              try {
+                data.map((values) => {
+                  data1 += `<div class="col-md-6 col-lg-4" id="${values.webinars_id}">
+                              <div class="blog-current-style1 mb-lg-30">
+                                <article class="post">
+                                  <div class="entry-header">
+                                    <div class="post-thumb">
+                                      <div class="post-thumb-inner">
+                                        <div class="thumb" style="border-radius:15px 15px 0px 0px ;">
+                                          <img class="w-100" src="images/blog/session1.jpeg" alt="Image" />
+                                        </div>
+                                        <div class="meta-date">${values.webinars_date}</div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                  <div class="entry-content" style="border-radius:0px 0px 15px 15px;">
+                                    <h6 class="entry-title mt-0">
+                                      <p>${values.webname}</p>
+                                      <p>Speaker: ${values.speaker_name}</p>
+                                    </h6>
+                                    <div class="post-btn-readmore">
+                                      <a href="${values.webinars_invitation}" class="btn btn-plain-text-with-arrow">Register Now!</a>
+                                    </div>
+                                  </div>
+                                </article>
+                              </div>
+                            </div>`
+                });
 
-                    <div class="meta-date">15 Jan</div>
-                  </div>
-                </div>
-              </div>
-              <div class="entry-content" style="border-radius:0px 0px 15px 15px;">
-                <h6 class="entry-title mt-0">
-                  <p>Greetings everyone✨!
-                    ADORE invites you to a Webinar on Pursuing your Interest in a Dynamic World by Mr. Chitresh Shrivastva
-                  </p>
-                </h6>
-                <div class="post-btn-readmore">
-                  <a href="https://zoom.us/j/7300060337?pwd=bWNPZ2JnS1o5TXNTVmhnVmM1L050Zz09" class="btn btn-plain-text-with-arrow">Register Now!</a>
-                </div>
-              </div>
-            </article>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-4">
-          <div class="blog-current-style1 mb-lg-30">
-            <article class="post">
-              <div class="entry-header">
-                <div class="post-thumb">
-                  <div class="post-thumb-inner">
-                    <div class="thumb" style="border-radius:15px 15px 0px 0px;">
-                      <img class="w-100" src="images/blog/session1.jpeg" alt="Image" />
-                    </div>
-                    <div class="meta-date">22 Jan</div>
-                  </div>
-                </div>
-              </div>
-              <div class="entry-content" style="border-radius:0px 0px 15px 15px;">
-                <h6 class="entry-title mt-0">
-                  <p>ADORE INDIA invites you for a Webinar on Empathy by Ms. Shalini Sahay</p>
-                </h6>
-                <div class="post-btn-readmore">
-                  <a href="https://zoom.us/j/3874658951" class="btn btn-plain-text-with-arrow">Register Now!</a>
-                </div>
-              </div>
-            </article>
-          </div>
-        </div>
+              } catch (err) {
+                console.log(err);
+              }
+              document.getElementById("webinar").innerHTML = data1;
+              console.log(data);
+            }).catch((error) => {
+              console.log(error);
+            });
+          } else {
+            // document.getElementsByClassName("preloader-floating-circles")[0].style.display = "none";
+            localStorage.getItem('webinars')
+            data = JSON.parse(localStorage.getItem('webinars'));
+            let data1 = "";
+            try {
+              console.log(data);
+              data.map((values) => {
+                data1 += `<div class="col-md-6 col-lg-4" id="${values.webinars_id}">
+                            <div class="blog-current-style1 mb-lg-30">
+                              <article class="post">
+                                <div class="entry-header">
+                                  <div class="post-thumb">
+                                    <div class="post-thumb-inner">
+                                      <div class="thumb" style="border-radius:15px 15px 0px 0px ;">
+                                        <img class="w-100" src="images/blog/session1.jpeg" alt="Image" />
+                                      </div>
+                                      <div class="meta-date">${values.webinars_date}</div>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div class="entry-content" style="border-radius:0px 0px 15px 15px;">
+                                  <h6 class="entry-title mt-0">
+                                    <p>${values.webname}</p>
+                                    </h6>
+                                    <p>Speaker: ${values.speaker_name}</p>
+                                  
+                                  <div class="post-btn-readmore">
+                                    <a href="${values.webinars_invitation}" class="btn btn-plain-text-with-arrow">Register Now!</a>
+                                  </div>
+                                </div>
+                              </article>
+                            </div>
+                          </div>`
+
+
+              });
+            } catch (err) {
+              console.log(err);
+            }
+            document.getElementById("webinar").innerHTML = data1;
+            console.log(data);
+          }
+        </script>
       </div>
     </div>
   </div>
