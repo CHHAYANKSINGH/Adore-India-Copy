e177af4e35e0902ac98e52fbb35f8c3f072aae4b
Shivvardhan <shivvardhan52@gmail.com>
2022-02-16 19:55:55 +0530

    Changed the hover color of contact anchor tag

--- index.php
+++ index.php
@@ -466,7 +466,7 @@
         <h4 style="color: white; margin-bottom: 60px">
           Come, join hands with us in fulfilling our social
           responsibility and spreading smiles. Email:
-          <a href="mailto:contact@adoreearth.org">contact@adoreearth.org<a />
+          <a class="contact_shiv_a" href="mailto:contact@adoreearth.org">contact@adoreearth.org</a>
         </h4>
         <a href="page-contact.php" style="border-radius:5px;width:120px;height:50px;padding-left:22px;padding-top:14px;" class="btn btn-theme-colored2 btn-flat">Contact Us</a>
       </div>
