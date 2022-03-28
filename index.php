7825b81bca5dce2ec23c91cf0d25d39f51759be4
Shivvardhan <shivvardhan52@gmail.com>
2022-03-28 20:14:54 +0530

    fixed the webinar error

--- index.php
+++ index.php
@@ -832,7 +832,7 @@
                         <a href="webinar.php" style="margin-top:15px;font-size:25px;border-radius:5px;width:205px;height:65px;padding-left:25px;padding-top:14px;" class="btn btn-theme-colored2 btn-flat">Our Webinars</a>
                     </div>
                 </div>
-                <div class="col-lg-6">
+                <div class="col-lg-7 ml-80" style="overflow:hidden;">
                     <div class="causes-carousel-current-style1">
                         <div class="tm-owl-carousel-2col owl-carousel owl-theme" id="webinar" data-autoplay="false" data-loop="true" data-duration="6000" data-smartspeed="300" data-margin="30" data-stagepadding="20" data-nav="true">
                             <script>
@@ -874,11 +874,12 @@
                                         data = _.filter(data, values => {
                                             return values.webinars_date >= setupDate
                                         });
-                                        console.log("webinar to show", data);
-                                        if (data.length == 0) {
-                                            //add no upcoming webinars section in data1 
-                                            // data1 += '<div><h2 class="title">No Upcoming Webinar</h2></div>'
-
+                                        var hello = data.length;
+                                    console.log("webinar to show", hello);
+                                        if (hello == 0) {
+                                            // add no upcoming webinars section in data1 
+                                            data1 += '<div><h2 class="title">No Upcoming Webinar</h2></div>'
+                                            
                                         }
                                         try {
                                             data.map((currentwebinar) => {
@@ -919,10 +920,12 @@
                                     data = _.filter(data, values => {
                                         return values.webinars_date >= setupDate
                                     });
-                                    console.log("webinar to show", data);
-                                    if (data.length == 0) {
-                                        //add no upcoming webinars section in data1 
-                                        // data1 += '<div><h2 class="title">No Upcoming Webinar</h2></div>'
+                                    var hello = data.length;
+                                    console.log("webinar to show", hello);
+                                    if (hello == 0) {
+                                        // add no upcoming webinars section in data1 
+                                        data1 += '<div><h2 class="title">No Upcoming Webinar</h2></div>'
+                                        
                                     }
                                     try {
                                         data.map((currentwebinar) => {
