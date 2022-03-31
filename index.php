f7c6598da97eba3604de43672fc1a8a20bb06950
Shivvardhan <shivvardhan52@gmail.com>
2022-03-31 15:36:45 +0530

    minor changes

--- index.php
+++ index.php
@@ -34,6 +34,26 @@
                   ">
                         <rs-module id="rev_slider_1_1" data-version="6.3.3">
                             <rs-slides>
+                            <rs-slide data-key="rs-13" data-title="Slide 1" data-thumb="images/3i.jpg" data-anim="ei:d;eo:d;s:d;r:default;t:slotslide-horizontal;sl:d;">
+                                    <img src="images/3i.jpg" title="firoox-s3" width="1920" height="1280" data-bg="p:center top;" data-parallax="off" class="rev-slidebg" data-no-retina />
+                                    <rs-layer id="slider-2-slide-12-layer-2" data-type="text" data-color="rgba(255, 255, 255, 0.11)" data-rsp_ch="on" data-xy="x:l,l,c,c;xo:0,26px,0,0;y:m;yo:-141px,-106px,-124px,-99px;" data-text="w:normal;s:180,150,130,100;l:165,98,90,72;ls:0,0,-1px,0px;fw:700;a:left,left,center,center;" data-dim="w:1200px,890px,537px,455px;h:186px,138px,auto,auto;" data-frame_0="o:1;" data-frame_0_chars="d:5;y:-100%;o:0;rZ:35deg;" data-frame_0_mask="u:t;" data-frame_1="st:1360;sp:1200;sR:1360;" data-frame_1_chars="e:power4.inOut;d:10;rZ:0deg;" data-frame_1_mask="u:t;" data-frame_999="o:0;st:w;sR:5640;" style="z-index: 13; text-transform: uppercase" class="font-current-theme1">Adore
+                                    </rs-layer>
+                                    <rs-layer id="slider-2-slide-12-layer-10" data-type="text" data-rsp_ch="on" data-xy="x:l,l,c,c;xo:55px,55px,0,0;yo:281px,203px,175px,179px;" data-text="w:normal;s:22,22,24,20;l:30,32,24,22;ls:1px,1px,0px,1px;fw:500;a:left,left,center,center;" data-dim="w:482px,355px,344px,356px;h:auto,auto,auto,26px;" data-frame_0="y:-50,-37,-28,-17;" data-frame_1="st:500;sp:1000;sR:500;" data-frame_999="o:0;st:w;sR:7500;" style="z-index: 12" class="font-current-theme1">Welcome to ADORE
+                                    </rs-layer>
+                                    <rs-layer id="slider-2-slide-11-layer-18" data-type="text" data-rsp_ch="on" data-xy="x:l,l,c,c;xo:55px,55px,8px,0;yo:318px,249px,212px,215px;" data-text="w:normal;s:75,70,60,40;l:95,90,80,50;ls:0px,0px,-1px,0px;fw:700;a:left,left,center,center;" data-dim="w:850px,810px,659px,455px;" data-frame_0="y:-50,-37,-28,-17;" data-frame_1="st:1100;sp:1000;sR:1100;" data-frame_999="o:0;st:w;sR:6900;" style="z-index: 11; text-transform: uppercase" class="mt-30 font-current-theme1">
+                                        <span class="text-theme-colored2">Empowering Youth</span>
+                                    </rs-layer>
+                                    <rs-layer id="slider-2-slide-2-layer-22" data-type="text" data-rsp_ch="on" data-xy="x:l,l,c,c;xo:60px,60px,-90px,-100px;yo:548px,480px,440px,370px;" data-text="w:normal;s:20,16,16,13;l:22,20,18,20;a:left,left,left,center;" data-frame_0="y:-50,-37,-28,-17;" data-frame_1="st:1700;sp:1000;sR:1700;" data-frame_999="o:0;st:w;sR:6300;" style="z-index: 10" class="font-current-theme1"><a href="about.php" style="font-weight:700;border-radius:5px;max-width:130px;max-height:50px;min-width:100px;min-height:35px;padding-left:20px;padding-right:22px;padding-top:14px;" class="btn btn-theme-colored1 btn-flat">Know More</a>
+                                    </rs-layer>
+                                    <rs-layer id="slider-2-slide-2-layer-3" data-type="text" data-rsp_ch="on" data-xy="x:l,l,c,c;xo:200px,200px,100px,110px;yo:548px,480px,440px,370px;" data-text="w:normal;s:20,16,16,13;l:22,20,18,20;a:left,left,left,center;" data-frame_0="y:-50,-37,-28,-17;" data-frame_1="st:2020;sp:1000;sR:2020;" data-frame_999="o:0;st:w;sR:5980;" style="z-index: 9" class="font-current-theme1"><a href="page-contact.php" style="font-weight:700;border-radius:5px;max-width:140px;max-height:50px;min-width:100px;min-height:35px;padding-left:22px;padding-right:22px; padding-top:14px;" class="btn btn-theme-colored2 btn-flat">Volunteer Us</a>
+                                    </rs-layer>
+                                    <rs-layer id="slider-2-slide-12-layer-0" data-type="shape" data-rsp_ch="on" data-text="w:normal;s:20,14,10,6;l:0,18,13,8;" data-dim="w:100%;h:100%;" data-basealign="slide" data-frame_999="o:0;st:w;sR:8700;" style="
+                            z-index: 8;
+                            background-color: rgba(0, 10, 22, 0.17);
+                          ">
+                                    </rs-layer>
+                                </rs-slide>
+                            
                                 <rs-slide data-key="rs-2" data-title="Slide 1" data-thumb="images/1.jpg" data-anim="ei:d;eo:d;s:d;r:default;t:slotslide-horizontal;sl:d;">
                                     <img src="images/1.jpg" title="firoox-s1" width="1920" height="1280" data-bg="p:center top;" data-parallax="off" class="rev-slidebg" data-no-retina />
                                     <rs-layer id="slider-2-slide-2-layer-2" data-type="text" data-color="rgba(255, 255, 255, 0.11)" data-rsp_ch="on" data-xy="x:l,l,c,c;xo:0,26px,0,0;y:m;yo:-141px,-103px,-124px,-99px;" data-text="w:normal;s:180,150,130,100;l:165,98,90,72;ls:0,0,-1px,0px;fw:700;a:left,left,center,center;" data-dim="w:1200px,890px,537px,455px;h:186px,138px,auto,auto;" data-frame_0="o:1;" data-frame_0_chars="d:5;y:-100%;o:0;rZ:35deg;" data-frame_0_mask="u:t;" data-frame_1="st:1360;sp:1200;sR:1360;" data-frame_1_chars="e:power4.inOut;d:10;rZ:0deg;" data-frame_1_mask="u:t;" data-frame_999="o:0;st:w;sR:5640;" style="z-index: 13; text-transform: uppercase;" class="font-current-theme1">
@@ -92,25 +112,7 @@
                           ">
                                     </rs-layer>
                                 </rs-slide>
-                                <rs-slide data-key="rs-13" data-title="Slide 1" data-thumb="images/3i.jpg" data-anim="ei:d;eo:d;s:d;r:default;t:slotslide-horizontal;sl:d;">
-                                    <img src="images/3i.jpg" title="firoox-s3" width="1920" height="1280" data-bg="p:center top;" data-parallax="off" class="rev-slidebg" data-no-retina />
-                                    <rs-layer id="slider-2-slide-12-layer-2" data-type="text" data-color="rgba(255, 255, 255, 0.11)" data-rsp_ch="on" data-xy="x:l,l,c,c;xo:0,26px,0,0;y:m;yo:-141px,-106px,-124px,-99px;" data-text="w:normal;s:180,150,130,100;l:165,98,90,72;ls:0,0,-1px,0px;fw:700;a:left,left,center,center;" data-dim="w:1200px,890px,537px,455px;h:186px,138px,auto,auto;" data-frame_0="o:1;" data-frame_0_chars="d:5;y:-100%;o:0;rZ:35deg;" data-frame_0_mask="u:t;" data-frame_1="st:1360;sp:1200;sR:1360;" data-frame_1_chars="e:power4.inOut;d:10;rZ:0deg;" data-frame_1_mask="u:t;" data-frame_999="o:0;st:w;sR:5640;" style="z-index: 13; text-transform: uppercase" class="font-current-theme1">Adore
-                                    </rs-layer>
-                                    <rs-layer id="slider-2-slide-12-layer-10" data-type="text" data-rsp_ch="on" data-xy="x:l,l,c,c;xo:55px,55px,0,0;yo:281px,203px,175px,179px;" data-text="w:normal;s:22,22,24,20;l:30,32,24,22;ls:1px,1px,0px,1px;fw:500;a:left,left,center,center;" data-dim="w:482px,355px,344px,356px;h:auto,auto,auto,26px;" data-frame_0="y:-50,-37,-28,-17;" data-frame_1="st:500;sp:1000;sR:500;" data-frame_999="o:0;st:w;sR:7500;" style="z-index: 12" class="font-current-theme1">Welcome to ADORE
-                                    </rs-layer>
-                                    <rs-layer id="slider-2-slide-11-layer-18" data-type="text" data-rsp_ch="on" data-xy="x:l,l,c,c;xo:55px,55px,8px,0;yo:318px,249px,212px,215px;" data-text="w:normal;s:75,70,60,40;l:95,90,80,50;ls:0px,0px,-1px,0px;fw:700;a:left,left,center,center;" data-dim="w:850px,810px,659px,455px;" data-frame_0="y:-50,-37,-28,-17;" data-frame_1="st:1100;sp:1000;sR:1100;" data-frame_999="o:0;st:w;sR:6900;" style="z-index: 11; text-transform: uppercase" class="mt-30 font-current-theme1">
-                                        <span class="text-theme-colored2">Empowering Youth</span>
-                                    </rs-layer>
-                                    <rs-layer id="slider-2-slide-2-layer-22" data-type="text" data-rsp_ch="on" data-xy="x:l,l,c,c;xo:60px,60px,-90px,-100px;yo:548px,480px,440px,370px;" data-text="w:normal;s:20,16,16,13;l:22,20,18,20;a:left,left,left,center;" data-frame_0="y:-50,-37,-28,-17;" data-frame_1="st:1700;sp:1000;sR:1700;" data-frame_999="o:0;st:w;sR:6300;" style="z-index: 10" class="font-current-theme1"><a href="about.php" style="font-weight:700;border-radius:5px;max-width:130px;max-height:50px;min-width:100px;min-height:35px;padding-left:20px;padding-right:22px;padding-top:14px;" class="btn btn-theme-colored1 btn-flat">Know More</a>
-                                    </rs-layer>
-                                    <rs-layer id="slider-2-slide-2-layer-3" data-type="text" data-rsp_ch="on" data-xy="x:l,l,c,c;xo:200px,200px,100px,110px;yo:548px,480px,440px,370px;" data-text="w:normal;s:20,16,16,13;l:22,20,18,20;a:left,left,left,center;" data-frame_0="y:-50,-37,-28,-17;" data-frame_1="st:2020;sp:1000;sR:2020;" data-frame_999="o:0;st:w;sR:5980;" style="z-index: 9" class="font-current-theme1"><a href="page-contact.php" style="font-weight:700;border-radius:5px;max-width:140px;max-height:50px;min-width:100px;min-height:35px;padding-left:22px;padding-right:22px; padding-top:14px;" class="btn btn-theme-colored2 btn-flat">Volunteer Us</a>
-                                    </rs-layer>
-                                    <rs-layer id="slider-2-slide-12-layer-0" data-type="shape" data-rsp_ch="on" data-text="w:normal;s:20,14,10,6;l:0,18,13,8;" data-dim="w:100%;h:100%;" data-basealign="slide" data-frame_999="o:0;st:w;sR:8700;" style="
-                            z-index: 8;
-                            background-color: rgba(0, 10, 22, 0.17);
-                          ">
-                                    </rs-layer>
-                                </rs-slide>
+                                
                             </rs-slides>
                             <rs-static-layers>
                                 <!--
